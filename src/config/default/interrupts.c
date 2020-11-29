3ec23be18d8dabdf1cbc2f893a538cf83d8fced7
Luis Yague <luis.yague@vdi-e.es>
2020-11-29 13:04:30 +0100

    Two adcs working

--- src/config/default/interrupts.c
+++ src/config/default/interrupts.c
@@ -16,19 +16,27 @@ float Brake;
 void __ISR(_CORE_TIMER_VECTOR, ipl1SOFT) CORE_TIMER_Handler (void)
 {   
         //ADC_SamplingStart();
+    AD1CHS = 0x170000;
     ADC_InputSelect(ADC_MUX_A, ADC_INPUT_POSITIVE_AN23, ADC_INPUT_NEGATIVE_VREFL);
     ADC_ConversionStart();
     while(!ADC_ResultIsReady());
     adc_count = ADC_ResultGet(ADC_RESULT_BUFFER_0);
     Acc1 = (float)adc_count * ADC_VREF / ADC_MAX_COUNT;
     
+    AD1CHS = 0x180000;
     ADC_InputSelect(ADC_MUX_A, ADC_INPUT_POSITIVE_AN29, ADC_INPUT_NEGATIVE_VREFL);
     ADC_ConversionStart();
     while(!ADC_ResultIsReady());
     adc_count2 = ADC_ResultGet(ADC_RESULT_BUFFER_0);
     Brake = (float)adc_count2 * ADC_VREF / ADC_MAX_COUNT;
     
-    LATDINV = (1<<2);
+    if(Acc1 > 1.5){
+        LATDSET = (1<<2);
+    } else {
+        LATDCLR = (1<<2);
+    }
+    
+    //LATDINV = (1<<2);
     CORE_TIMER_InterruptHandler();
 }
 
